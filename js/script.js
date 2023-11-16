e79e0871733e869d138b7559b996ab0a40709c9c
cshrs <145676128+cshrs@users.noreply.github.com>
2023-11-16 15:55:53 +0000

    Scroll test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Scroll to top button
+
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopButton = document.querySelector(".scroll-to-top");
+
+    window.addEventListener("scroll", function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+    scrollToTopButton.addEventListener("click", function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+});
