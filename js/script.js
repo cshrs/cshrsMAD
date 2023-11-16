a70cecda4fbe62ae0274b0cb6871d39a60f83a37
cshrs <145676128+cshrs@users.noreply.github.com>
2023-11-16 16:07:15 +0000

    .

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 // Scroll to top button: From W3 Schools, ChatGPT and Stack Overflow
 
-document.addEventListener("DOMContentLoaded", function () {
-    var scrollToTopButton = document.querySelector(".scroll-to-top");
 
-    window.addEventListener("scroll", function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            scrollToTopButton.style.display = "block";
-        } else {
-            scrollToTopButton.style.display = "none";
-        }
-    });
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
-    scrollToTopButton.addEventListener("click", function () {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-    });
-});
+// Show or hide the scroll-to-top button based on scroll position
+window.onscroll = function () {
+    var scrollButton = document.querySelector(".scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
