6e94dd18af9276e4d117c68795763ced6f877383
cshrs <145676128+cshrs@users.noreply.github.com>
2023-11-18 16:54:39 +0000

    js change

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function scrollToTop() {
 // Show or hide the scroll-to-top button based on scroll position
 window.onscroll = function () {
     var scrollButton = document.querySelector(".scroll-to-top");
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         scrollButton.style.display = "block";
     } else {
         scrollButton.style.display = "none";
