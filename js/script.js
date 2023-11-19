eb2db184a55e76c369b08a7c9894df9c2f5e11b2
cshrs <145676128+cshrs@users.noreply.github.com>
2023-11-19 16:58:05 +0000

    .

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function scrollToTop() {
 // Show or hide the scroll-to-top button based on scroll position
 window.onscroll = function () {
     var scrollButton = document.querySelector(".scroll-to-top");
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         scrollButton.style.display = "block";
     } else {
         scrollButton.style.display = "none";
